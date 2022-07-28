e1d5e898c51646a5c4abbe67aef12f43dff7c208
EmanuelAlderete <aldereteema28@gmail.com>
2022-07-27 21:42:27 -0300

    v4 de estructura del proyecto

--- source/urls.py
+++ source/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from .views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Index,name='index'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
